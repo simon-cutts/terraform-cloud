4415b7bff4b2c283b60596052970701631563570
Simon Cutts <sighware@gmail.com>
2020-08-11 10:10:38 +0100

    Update variables.tf
    Add creds

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   type    = string
-  default = "us-west-1"
+  default = "eu-west-2"
 }
 
 variable "aws_access_key_id" {
-  type    = string
+  type    = "AKIAJL5NO7L3OQNLKE2Q"
 }
 
 variable "aws_secret_access_key" {
-  type    = string
+  type    = "YXP7lf6vi537k3Z4dUG5niIt8m/sdRpdZgf8+HHh"
 }
 
 variable "db_table_name" {
